0aa26fab0bcfe42088fa2d7cfc5993db96562ba5
bostonlink <bostonlink@pentest-labs.org>
2012-12-27 09:53:34 -0500

    edited and tested

--- nw_threat_2_filetype.py
+++ nw_threat_2_filetype.py
@@ -8,6 +8,7 @@
 
 import sys
 import urllib2, urllib, json
+from datetime import datetime, timedelta
 
 from lib import nwmodule
 
@@ -24,13 +25,19 @@ nwmodule.nw_http_auth()
 
 risk_name = sys.argv[1]
 fields = sys.argv[2].split('#')
+
+date_t = datetime.today()
+tdelta = timedelta(days=1)
+diff = date_t - tdelta
+diff = "'" + diff.strftime('%Y-%b-%d %H:%M:%S') + "'-'" + date_t.strftime('%Y-%b-%d %H:%M:%S') + "'"
+
 for i in fields:
     if 'ip' in i:
         parse = i.split('=')
         ip = parse[1]
-        where_clause = 'risk.warning="%s" && ip.src=%s || ip.dst=%s' % (risk_name, ip, ip)
+        where_clause = '(time=%s) && risk.warning="%s" && ip.src=%s || ip.dst=%s' % (diff, risk_name, ip, ip)
     else:
-        where_clause = 'risk.warning="%s"' % (risk_name)
+        where_clause = '(time=%s) && risk.warning="%s"' % (diff, risk_name)
 
 field_name = 'filetype'
 json_data = json.loads(nwmodule.nwValue(0, 0, 25, field_name, 'application/json', where_clause))
